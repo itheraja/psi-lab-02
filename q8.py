77f9d8ca10a88669cb8bac2489dfa7c2d90b1dde
Raja MT. <rajataha151@gmail.com>
2026-08-27 01:33:02 +0500

    Add script to filter unique emails from a list

---
+++ q8.py
@@ -0,0 +1,16 @@
+emails = [
+    "ali@gmail.com",
+    "sara@yahoo.com",
+    "ali@gmail.com",
+    "ahmed@gmail.com",
+    "sara@yahoo.com",
+    "zain@hotmail.com"
+]
+
+unique_emails = list(set(emails))
+
+print("Original List:")
+print(emails)
+
+print("\nUnique Emails:")
+print(unique_emails)
